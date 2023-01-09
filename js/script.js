09682d49f25b741b652e2e9d08d5b69cde3f1a3e
Gustavo <gsevillagarces@gmail.com>
2023-01-10 00:09:45 +0100

    Cambios generales de estilos y agregado contenido estático de modal

--- js/script.js
+++ js/script.js
@@ -1,37 +1,56 @@
+// ::: Script para mostrar NAVBAR :::
 // En este primer script, se muestra el navbar de acuerdo al scroll del usuario. Este navbar inicia oculto, y se muestra, con una pequeña transición dada en el css, si el usuario hace un scroll mayor a 50px.
+const nav = document.getElementById("navbar");
+let navbarH = nav.getBoundingClientRect().height;
 
-const nav = document.getElementById("navbar")
+console.log(`-${navbarH}px`);
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+function showNav() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) { //50 es el número de px que deben darse con el scroll para que se muestre el elemento
     nav.style.top = "0";
-  } else {
-    nav.style.top = "-115px";
+    } else {
+    nav.style.top = `-${navbarH}px`; //El alto tomado  del navbar, para ocultarlo correctamente
   }
 }
 
-window.onscroll = function() {scrollFunction()};
+window.addEventListener("scroll", showNav);
+// ---
 
 
-// Atrapo el modal
+
+// ::: Script para controlar el MODAL :::
 var modal = document.querySelector(".modal");
-
-// Atrapo el btn (en este caso los botones) que abrirán el modal
 var btn = document.querySelectorAll(".btnModal");
+var close = document.querySelector(".modal-close");
 
-// Atrapo el elemento que cerrará el modal
-var close = document.querySelector(".close-modal");
+btn.forEach(boton => boton.addEventListener("click", () => modal.style.display = "block")); // Abrir el modal al dar click en uno de los botones que están para esta acción 
 
-// When the user clicks on the button, open the modal 
-btn.forEach(boton => boton.addEventListener("click", () => modal.style.display = "block"));
+close.addEventListener("click", () => modal.style.display = "none"); // Cerrar el modal al dar click en el botón (X)
 
-// When the user clicks on <span> (x), close the modal
+window.addEventListener("click", (e) => {
+    if (e.target == modal) {  // Cerrar el modal al dar click fuera del modal
+        modal.style.display = "none";
+    }
+});
+// ---
 
-close.addEventListener("click", () => modal.style.display = "none");
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+
+// ::: Script para dar una pequeña animación de entrada a los elementos al ir bajando por la página :::
+var revealElements = document.querySelectorAll(".reveal");
+
+function reveal() {
+    for (var i = 0; i < revealElements.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = revealElements[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+    
+            if (elementTop < windowHeight - elementVisible) {
+                revealElements[i].classList.add("active");
+            } else {
+                revealElements[i].classList.remove("active");
+            }
+    }
 }
+
+window.addEventListener("scroll", reveal);
