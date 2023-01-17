82e181516bd24656a470c91c917c3f6631a2c3c3
Gustavo <gsevillagarces@gmail.com>
2023-01-17 22:47:09 +0100

    Cambios en img de montaditos y fix en montadito selected

--- js/script.js
+++ js/script.js
@@ -50,8 +50,8 @@ var montaditoTxt = document.querySelector(".num-select-montadito");
 var btnMontaditoRdm = document.querySelectorAll(".btnRdm");
 
 let montaditos = {
-    imgs : ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18", "img19", "img20", "img21", "img22", "img23", "img24", "img25"],
-    nums : ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"]
+    imgs : ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8", "img9", "img10", "img11", "img12", "img13", "img14", "img15", "img16", "img17", "img18", "img19", "img20"],
+    nums : ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 };
 
 function montaditoRandom (){
